17d1688e29b7f2cc984cd9d8ba2ede2ec4ebbf80
Tyler Anton <mtyleranton@gmail.com>
2017-07-21 18:04:49 -0500

    minor thing

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -4,7 +4,7 @@ import { Todoist } from './todoist';
 import createPanel from '../components/panel/Panel';
 
 let plugins = [
-  createPanel(Twitter, { width: '250px', height: '475px' }),
+  createPanel(Twitter, { width: '250px', height: '475px', bgColor: 'pink' }),
   createPanel(Todoist, { resize: 'both' }),
   createPanel(Test, { width: '350px', height: '300px', resize: 'both' })
 ];
